dac9998ede0f37664d3886a671d1ef4312fdc13e
Rod Scott <rjsc@ceh.ac.uk>
2018-06-25 16:27:10 +0100

    Read csv file of results
    master

--- results.py
+++ results.py
@@ -1,28 +1,52 @@
-import sys
 import argparse
+import csv
+import sys
 
 
 def main():
     check_python3()
-    category, team_size = capture_commandline_args()
+    filename, category, team_size = capture_commandline_args()
     print(
-        'Category: {category}, Team size: {team_size}'
-        .format(category=category, team_size=team_size)
+        'Filename: {filename}, Category: {category}, Team size: {team_size}'
+        .format(filename=filename, category=category, team_size=team_size)
     )
+    print(read_results_csv_file('fixtures/results.csv'))
+    print({'MSEN': [
+        {'club': 'A', 'counters': [('A1', 2), ('A2', 3), ('A3', 5)], 'score': 10},
+        {'club': 'B', 'counters': [('B1', 1), ('B2', 4), ('B3', 6)], 'score': 11}
+    ]})
 
 
 def check_python3():
-    if sys.version_info[0] < 3:
-        exit('ERROR: Python 3 or greater required')
+    if sys.version_info[0] < 3 and sys.version_info[1] < 6:
+        exit('ERROR: Python 3.6 or greater required')
 
 
 def capture_commandline_args():
     parser = argparse.ArgumentParser(description='Calculate team results for a fell race')
-    parser.add_argument('-c', '--category', help='category of team', default='MSEN')
-    parser.add_argument('-t', '--team-size', type=int, help='size of team', default=3)
+    parser.add_argument('-f', '--filename', help='Results filename in csv format. Expected columns: position, name, category, club, time')
+    parser.add_argument('-c', '--category', help='Category of team', default='MSEN')
+    parser.add_argument('-t', '--team-size', type=int, help='Size of team', default=3)
 
     args = parser.parse_args()
-    return args.category, args.team_size
+    return args.filename, args.category, args.team_size
+
+
+def read_results_csv_file(filename):
+    to_return = []
+    with open(filename) as resultsfile:
+        has_header = csv.Sniffer().has_header(resultsfile.read(1024))
+        resultsfile.seek(0)
+        reader = csv.DictReader(resultsfile, fieldnames=['position', 'name', 'category', 'club', 'time'], restkey='extra')
+        if has_header:
+            next(reader) # skip column names row 
+        for row in reader:
+            to_return.append(row)
+    return to_return
+
+
+def get_teams(team_size, category):
+    []
 
 
 if __name__ == "__main__":
--- test_results.py
+++ test_results.py
@@ -3,15 +3,19 @@ import results
 
 
 def test_default_commandline_arguments():
-    with patch('sys.argv', ['prog']):
-        assert ('MSEN', 3) == results.capture_commandline_args()
+    with patch('sys.argv', ['prog', '-f', 'results.csv']):
+        assert ('results.csv', 'MSEN', 3) == results.capture_commandline_args()
 
 
 def test_non_default_using_short_commandline_arguments():
-    with patch('sys.argv', ['prog', '-c', 'W40', '-t', '4']):
-        assert ('W40', 4) == results.capture_commandline_args()
+    with patch('sys.argv', ['prog', '-f', 'race-results.csv', '-c', 'W40', '-t', '4']):
+        assert ('race-results.csv', 'W40', 4) == results.capture_commandline_args()
 
 
 def test_non_default_using_long_commandline_arguments():
-    with patch('sys.argv', ['prog', '--category', 'M60', '--team-size', '8']):
-        assert ('M60', 8) == results.capture_commandline_args()
+    with patch('sys.argv', ['prog','--filename', 'race.csv', '--category', 'M60', '--team-size', '8']):
+        assert ('race.csv', 'M60', 8) == results.capture_commandline_args()
+
+
+def test_read_results_csv_file():   
+    assert 14 == len(results.read_results_csv_file('fixtures/results.csv'))
